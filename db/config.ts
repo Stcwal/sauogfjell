c855d8a22c8d7501ff35f97547a8b5e380287f93
anderrfa <anderrfa@ntnu.no>
2026-06-02 21:27:19 +0200

    feat(turso) add turso and update docs to reflect updates

--- db/config.ts
+++ db/config.ts
@@ -1,5 +1,5 @@
 // https://docs.turso.tech/introduction
-//https://docs.astro.build/en/guides/astro-db/
+// https://docs.astro.build/en/guides/astro-db/
 
 import { defineDb, defineTable, column } from 'astro:db';
 
